39ada72f691ebaef47fe383d0367916d8bffd30b
Priscila Oliveira <priscila.oliveira@sentry.io>
2021-08-12 01:05:08 +0200

    feat(episodes): Add episodes page design

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import {Header} from './components/header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header className="App-header" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,5 @@
+function Header() {
+  return <nav>OIIIcccc I</nav>;
+}
+
+export { Header };

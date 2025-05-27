d6556d4cc47d183b0a0d434518a6e2938de11d27
John Manhart <manhartjohn@gmail.com>
2025-05-27 12:29:51 -0700

    Adding in styles for the header and countdown timer

--- src/components/PageHeader/index.js
+++ src/components/PageHeader/index.js
@@ -5,7 +5,7 @@ const PageHeader = ({title, currentYear, className = '', ...props}) => (
   <div>
     <div style={{display: 'flex', alignItems: 'baseline', gap: '0.5em'}}>
       {title && <h2 className="page-header-title">{title}</h2>}
-      {currentYear && <span>{currentYear}</span>}
+      {currentYear && <h2>{currentYear}</h2>}
     </div>
     <hr class="squiggle-line" />
   </div>
